e88e72e747fffc6a6b75aa76b5041432aa124b5a
rodrigo73garcia <texchrg@gmail.com>
2025-11-04 15:13:17 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ import cors from "cors";
 import bodyParser from "body-parser";
 import { PORT, TRANSLATION_LANGUAGES } from "./config.js";
 import { fetchAndTranslateSubtitle } from "./subtitles.js";
+import { generateManifest } from "./manifest.js";
 
 const app = express();
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static("public"));
@@ -14,11 +16,17 @@ app.get("/", (req, res) => {
   res.sendFile(process.cwd() + "/public/index.html");
 });
 
+// ROTA DO MANIFEST
+app.get("/manifest.json", (req, res) => {
+  const lang = req.query.lang || "pt-br";
+  const manifest = generateManifest(lang);
+  res.json(manifest);
+});
+
 // Endpoint principal de legendas para Stremio
 app.get("/subtitles/movie/:imdbId.json", async (req, res) => {
   const { imdbId } = req.params;
   const lang = req.query.lang || "pt-br";
-
   const result = await fetchAndTranslateSubtitle(imdbId, lang);
   if (!result) {
     return res.json({ subtitles: [] });
